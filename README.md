0c4de2a8d7f223487331a444bc87f75c88fc4f20
aq_minato.naka <minato.naka@asia-quest.jp>
2019-12-10 10:25:40 +0900

    初期コミット

---
+++ README.md
@@ -0,0 +1,16 @@
+フロントエンドプロジェクト作成のスケルトン
+
+# パッケージ
+下記のパッケージが利用可能な状態
+
+* bootstrap
+* fontawesome
+
+# ビルド
+* パッケージインストール  
+`npm install`  
+  
+* ビルド  
+`npm run dev`  
+※Laravel Mixを使用  
+
---
+++ webpack.mix.js
@@ -0,0 +1,27 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.sass('src/scss/bootstrap.scss', 'dist/build/css')
+    .sass('src/scss/fontawesome.scss', 'dist/build/css')
+    .sass('src/scss/app.scss', 'dist/build/css')
+    .styles([
+        'dist/build/css/bootstrap.css',
+        'dist/build/css/fontawesome.css',
+        'dist/build/css/app.css',
+    ], 'dist/build/css/bundle.css')
+    .scripts([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'src/js/app.js',
+    ], 'dist/build/js/bundle.js');
